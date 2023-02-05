725ab19c236f6d397b421534094997556ee03010
issam <aissam.16.25@gmail.com>
2023-02-05 22:42:43 +0100

    add components and pages folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline bg-red-600">
-     React Dashboard
-    </div>
+    
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+          <Route index element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+     
+      </BrowserRouter>
+   
   );
 }
 
---
+++ src/pages/AdminListing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminListing = () => {
+  return (
+    <div>AdminListing</div>
+  )
+}
+
+export default AdminListing
